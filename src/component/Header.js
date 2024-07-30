0af8f9825462ea92b00a32edf41c9cb84eef3aaa
Slim Shady <asterix1515@hotmail.com>
2024-07-30 00:27:06 -0400

    Add new components and update existing files

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* Contenu du header */}
+      <h1>Homepage</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/component/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <Main>
+      {/* Contenu du Main */}
+    </Main>
+  );
+}
+
+export default Main;
